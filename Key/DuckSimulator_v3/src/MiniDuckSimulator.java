dead7467cd9e4c97b96471321fa1bcb4e6fa42ae
Larry Clement <lclement@calbaptist.edu>
2024-04-02 17:38:36 -0700

    After class on 4/2/2024

--- Key/DuckSimulator_v3/src/MiniDuckSimulator.java
+++ Key/DuckSimulator_v3/src/MiniDuckSimulator.java
@@ -3,8 +3,20 @@ public class MiniDuckSimulator {
         Duck[] ducksInSimulator = new Duck[]{
                 new MallardDuck(),
                 new RedheadDuck(),
-                new RubberDuck()
+                new RubberDuck(),
+                new DecoyDuck()
         };
+
+        for (Duck duck : ducksInSimulator) {
+            duck.display();
+            duck.performQuack();
+            duck.swim();
+            duck.performFly();
+            System.out.println();
+        }
+
+        ducksInSimulator[3].setFlyBehavior(new FlyWithRocketPower());
+
 
         for (Duck duck : ducksInSimulator) {
             duck.display();
